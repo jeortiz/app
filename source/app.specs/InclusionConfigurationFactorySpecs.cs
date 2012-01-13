f87722f3080aaea2891b6b2e1b994a15a07238e2
juan <jortiz@intellisys.com.do>
2012-01-13 14:44:07 -0400

    Committing

---
+++ source/app.specs/InclusionConfigurationFactorySpecs.cs
@@ -0,0 +1,38 @@
+﻿ using Machine.Specifications;
+ using app.web.core.urls;
+ using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+
+    [Subject(typeof(InclusionConfigurationsFactory))]
+     public class InclusionConfigurationFactorySpecs
+     {
+         public abstract class concern :  Observes<ICreateInclusionConfigurators,
+                                              InclusionConfigurationsFactory>
+         {
+        
+         }
+
+         public class when_observation_name : concern
+         {
+             Establish c = () =>
+                             {
+            
+                             };
+
+             Because b = () =>
+                             {
+        
+                             };
+
+        
+             It first_observation = () =>
+                        {
+                            
+            
+            
+                        };
+         }
+     }
+ }
